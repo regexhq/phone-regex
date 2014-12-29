b97e0dc7a97b9ed76bd9e376b8c2e2fe28c929c8
John Otander <johnotander@gmail.com>
2014-12-29 14:10:15 -0700

    Add options for exact or g regex

--- index.js
+++ index.js
@@ -1,5 +1,9 @@
 'use strict';
 
-module.exports = function() {
-  return new RegExp('^\\s*(?:\\+?(\\d{1,3}))?[-. (]*(\\d{3})[-. )]*(\\d{3})[-. ]*(\\d{4})(?: *x(\\d+))?\\s*$');
+module.exports = function(options) {
+  options = options || {};
+  var regexBase = '\\s*(?:\\+?(\\d{1,3}))?[-. (]*(\\d{3})[-. )]*(\\d{3})[-. ]*(\\d{4})(?: *x(\\d+))?\\s*';
+
+  return options.exact ? new RegExp('^' + regexBase + '$') :
+                         new RegExp(regexBase, 'g');
 }
