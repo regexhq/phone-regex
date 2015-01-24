28ffc130964294918093283248b98fd7cd5757dd
John Otander <johnotander@gmail.com>
2015-01-24 11:32:31 -0700

    Don't allow leading or trailing whitespace for the exact regex option

--- index.js
+++ index.js
@@ -2,8 +2,8 @@
 
 module.exports = function(options) {
   options = options || {};
-  var regexBase = '\\s*(?:\\+?(\\d{1,3}))?[-. (]*(\\d{3})[-. )]*(\\d{3})[-. ]*(\\d{4})(?: *x(\\d+))?\\s*';
+  var regexBase = '(?:\\+?(\\d{1,3}))?[-. (]*(\\d{3})[-. )]*(\\d{3})[-. ]*(\\d{4})(?: *x(\\d+))?';
 
   return options.exact ? new RegExp('^' + regexBase + '$') :
-                         new RegExp(regexBase, 'g');
+                         new RegExp('\\s*' + regexBase + '\\s*', 'g');
 }
