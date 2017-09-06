2370d7c3c373ec79e49b7d15bd32f595887e4f8d
livin21 <livinmathew99@gmail.com>
2017-09-07 03:50:19 +0530

    Add Suport for Indian Numbers

--- index.js
+++ index.js
@@ -3,7 +3,9 @@
 module.exports = function(options) {
   options = options || {};
   var regexBase = '(?:\\+?(\\d{1,3}))?[-. (]*(\\d{3})[-. )]*(\\d{3})[-. ]*(\\d{4})(?: *x(\\d+))?';
+  var indianRegexBase = '(?:(?:\\+|0{0,2})91(\\s*[\\ -]\\s*)?|[0]?)?[789]\\d{9}|(\\d[ -]?){10}\\d';
 
-  return options.exact ? new RegExp('^' + regexBase + '$') :
+  return options.indian ? options.exact ? new RegExp('^' + indianRegexBase + '$') :
+                         new RegExp('\\s*' + indianRegexBase + '\\s*', 'g') : options.exact ? new RegExp('^' + regexBase + '$') :
                          new RegExp('\\s*' + regexBase + '\\s*', 'g');
 }
